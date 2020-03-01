19721a6047611e07ebc7526f1bc8d836b1b6e695
jordan-tiona <tionajordan@gmail.com>
2020-03-01 16:02:47 -0600

    Updates

---
+++ precache-manifest.6f1b8a910eb3dbc59a788e6b6d4b3213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31302a18da9a3816fa362a10ccb821e",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "d26498d1af5586c79209",
+    "url": "/react-calc/static/css/main.f2605aa8.chunk.css"
+  },
+  {
+    "revision": "61229532d0d321d744ad",
+    "url": "/react-calc/static/js/2.40150a44.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-calc/static/js/2.40150a44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d26498d1af5586c79209",
+    "url": "/react-calc/static/js/main.c5e50a2a.chunk.js"
+  },
+  {
+    "revision": "93eeaccac9bbcff7ae1b",
+    "url": "/react-calc/static/js/runtime-main.59620824.js"
+  }
+]);
